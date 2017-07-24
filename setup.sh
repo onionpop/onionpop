b8510c079b3c4679cc3ca7b43a98d34cd03d814f
mjuarezm <marc.juarez@kuleuven.be>
2017-07-24 11:25:15 -0400

    fix setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@
 pip install -r requirements.txt
 
 # install Arborist (Random Forest API)
-git clone git@github.com:mjuarezm/Arborist.git
+git clone https://github.com/onionpop/Arborist
 cd Arborist/ArboristBridgePy/
 pip install -r requirements.txt  # Arborist requirements
 python setup.py install
