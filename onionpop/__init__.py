f1b7f698f7dbd7b1392768eafe8f5bdfce5c230c
Rob Jansen <jansen@cs.umn.edu>
2017-07-25 16:03:16 -0400

    update package setup

---
+++ onionpop/__init__.py
@@ -0,0 +1,7 @@
+__all__ = [
+   'classifiers',
+   'cumul',
+   'features',
+   'kfp',
+   'pipeline',
+]
--- setup.py
+++ setup.py
@@ -1,22 +1,23 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+from distutils.core import setup
 
 setup(
     name="onionpop",
+    version=1.0,
+    description=("onionpop API to gather statistics in PrivCount."),
+    url='https://github.com/onionpop',
     author="mjuarezm",
     author_email="marc.juarez@kuleuven.be",
-    description=("onionpop API to gather statistics in PrivCount."),
     keywords=['tor', 'privacy', 'measurements', 'website fingerprinting'],
-    version=1.0,
     packages=['onionpop'],
-    setup_requires=['numpy', 'scipy'],
-    install_requires=[
-        'numpy',
-        'scipy',
-        'pandas',
-        'dill',
-		'cython>=0.24',
-        'scikit-learn',
-    ]
+    #setup_requires=['numpy', 'scipy'],
+    #install_requires=[
+    #    'numpy',
+    #    'scipy',
+    #    'pandas',
+    #    'dill',
+	#	'cython>=0.24',
+    #    'scikit-learn',
+    #]
 )
